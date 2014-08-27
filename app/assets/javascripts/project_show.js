16302fb457d87ed2891991f9e0bae34c57b965ae
Tsubasa Furukawa <tfurukawa824@gmail.com>
2014-08-27 00:11:00 -0700

    Second draft of project show page - mostly styling (responsive bulletin board layout)

--- app/assets/javascripts/project_show.js
+++ app/assets/javascripts/project_show.js
@@ -3,4 +3,12 @@ $(document).ready(function() {
     wrap: 'letter',
     watch: true
   });
+
+  // Set height of bulletin-board
+  setInterval(setHeight, 500)
 });
+
+var setHeight = function() {
+  var $bulletinContentHeight = $(".bulletin-board-content").css("height");
+  $(".bulletin-board-wrapper > img").height($bulletinContentHeight);
+};
