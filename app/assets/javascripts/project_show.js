fe8ec26119f3e42f1c4b5a9015edb7c2a3e557d3
Tsubasa Furukawa <tfurukawa824@gmail.com>
2014-09-05 23:02:56 -0700

    Slight design change for project show page

--- app/assets/javascripts/project_show.js
+++ app/assets/javascripts/project_show.js
@@ -3,24 +3,19 @@ $(document).ready(function() {
   $("#project-show .text-container").dotdotdot({
     wrap: 'letter',
     watch: true,
-    after: "<a class='readmore'>Read More</a>"
   });
 
-  $("#project-show .readmore").on("click", function(e) {
-    e.preventDefault();
+  $("#project-show .readmore").on("click", function() {
     alert("hello there");
   });
 
-  // Set height of bulletin-board
-  setInterval(setHeight, 500)
+  $("#project-show .hello").click(function() {
+    alert("hello there howdy");
+    return false;
+  });
 
   // // Popover
   // $("#project-show .wrapper").popover({
   //   content: $("#more-info-description").text()
   // });
 });
-
-var setHeight = function() {
-  var $bulletinContentHeight = $("#bulletin-board-content").css("height");
-  $("#board-background").height($bulletinContentHeight);
-};
