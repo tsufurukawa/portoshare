e41b89d5823faa36baaf534c5340c468ba1a3983
Tsubasa Furukawa <tfurukawa824@gmail.com>
2014-09-18 10:05:41 -0700

    Small typo fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@ CarrierWave.configure do |config|
   if Rails.env.staging? || Rails.env.production?
     config.storage = :fog
     config.fog_credentials = {
-      :provider               => Figar.env.FOG_PROVIDER,
+      :provider               => Figaro.env.FOG_PROVIDER,
       :aws_access_key_id      => Figaro.env.AWS_ACCESS_KEY_ID,
       :aws_secret_access_key  => Figaro.env.AWS_SECRET_ACCESS_KEY,
       :region                 => Figaro.env.FOG_REGION
