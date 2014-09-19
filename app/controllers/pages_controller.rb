22f9a5d302f0a5b013b3a4984eac1303f16b00dd
Tsubasa Furukawa <tfurukawa824@gmail.com>
2014-09-19 16:57:03 -0700

    Add touches to front page UI

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,6 @@
 class PagesController < ApplicationController
-  def front; end
+  def front
+    @sample_user = User.find_by(username: "tsufurukawa")
+    @sample_project = Project.find_by(title: "PortoShare")
+  end
 end
